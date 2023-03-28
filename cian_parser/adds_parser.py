8fe6454a7473619a7009368bd90783bc5ccfbf98
Frolov Kirill <frolovdata0238@gmail.com>
2023-03-28 14:36:13 +0700

    Prepared cian scrapper

--- cian_parser/adds_parser.py
+++ cian_parser/adds_parser.py
@@ -1,82 +1,82 @@
-import datetime
-import itertools
-import os
-import re
-import time
-from bs4 import BeautifulSoup
-import cloudscraper
-import logging
-from tqdm import tqdm
-from config import (
-    MAX_COUNT_OF_ADDS,
-    MAX_COUNT_OF_PAGE,
-    HEADERS,
-    START_URL,
-    ROOMS_MATCHING_DICT,
-    LOGS_PATH,
-    SCRAPE_LOG_PATH,
-    DATA_PATH
-    )
+# import datetime
+# import itertools
+# import os
+# import re
+# import time
+# from bs4 import BeautifulSoup
+# import cloudscraper
+# import logging
+# from tqdm import tqdm
+# from config import (
+#     MAX_COUNT_OF_ADDS,
+#     MAX_COUNT_OF_PAGE,
+#     HEADERS,
+#     START_URL,
+#     ROOMS_MATCHING_DICT,
+#     LOGS_PATH,
+#     SCRAPE_LOG_PATH,
+#     DATA_PATH
+#     )
 
-class CianUrlsParser:
-    def __init__(self, rooms: str) -> None:
-        """_summary_
+# class CianUrlsParser:
+#     def __init__(self, rooms: str) -> None:
+#         """_summary_
 
-        Args:
-            rooms (str): _description_
-        """
-        self.__MAX_COUNT_OF_ADDS = MAX_COUNT_OF_ADDS
-        self.__MAX_COUNT_OF_PAGE = MAX_COUNT_OF_PAGE
-        self.__MAX_COUNT_OF_PROPERTIES = self.__MAX_COUNT_OF_PAGE * self.__MAX_COUNT_OF_ADDS
-        self.keep_data_path = DATA_PATH
-        self.logs_path = LOGS_PATH
-        self.rooms_url = ROOMS_MATCHING_DICT[rooms]
-        self.scrape_log_file = SCRAPE_LOG_PATH
-        self.scrapper = cloudscraper.CloudScraper()
-        self.scrapper.headers = HEADERS
-        self.start_url = START_URL + self.rooms_url
+#         Args:
+#             rooms (str): _description_
+#         """
+#         self.__MAX_COUNT_OF_ADDS = MAX_COUNT_OF_ADDS
+#         self.__MAX_COUNT_OF_PAGE = MAX_COUNT_OF_PAGE
+#         self.__MAX_COUNT_OF_PROPERTIES = self.__MAX_COUNT_OF_PAGE * self.__MAX_COUNT_OF_ADDS
+#         self.keep_data_path = DATA_PATH
+#         self.logs_path = LOGS_PATH
+#         self.rooms_url = ROOMS_MATCHING_DICT[rooms]
+#         self.scrape_log_file = SCRAPE_LOG_PATH
+#         self.scrapper = cloudscraper.CloudScraper()
+#         self.scrapper.headers = HEADERS
+#         self.start_url = START_URL + self.rooms_url
         
-    def get_count_of_pages(self) -> int:
-        """_summary_
+#     def get_count_of_pages(self) -> int:
+#         """_summary_
 
-        Returns:
-            int: _description_
-        """
-        page = self.scrapper.get(url = self.start_url)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        text_count_page = soup.find('h5', {'class': '_93444fe79c--color_black_100--kPHhJ _93444fe79c--lineHeight_20px--tUURJ _93444fe79c--fontWeight_bold--ePDnv _93444fe79c--fontSize_14px--TCfeJ _93444fe79c--display_block--pDAEx _93444fe79c--text--g9xAG _93444fe79c--text_letterSpacing__normal--xbqP6'}).text
-        count_of_adds = int(''.join(re.findall(r'\d+', text_count_page)))
-        count_of_pages = count_of_adds // 50 + 1
-        return count_of_pages
+#         Returns:
+#             int: _description_
+#         """
+#         page = self.scrapper.get(url = self.start_url)
+#         soup = BeautifulSoup(page.text, 'html.parser')
+#         text_count_page = soup.find('h5', {'class': '_93444fe79c--color_black_100--kPHhJ _93444fe79c--lineHeight_20px--tUURJ _93444fe79c--fontWeight_bold--ePDnv _93444fe79c--fontSize_14px--TCfeJ _93444fe79c--display_block--pDAEx _93444fe79c--text--g9xAG _93444fe79c--text_letterSpacing__normal--xbqP6'}).text
+#         count_of_adds = int(''.join(re.findall(r'\d+', text_count_page)))
+#         count_of_pages = count_of_adds // 50 + 1
+#         return count_of_pages
 
-    def get_info_about_adds(self) -> None:
-        """_summary_
-        """
-        count_of_pages = get_count_of_pages()
-        ret = f"Count of pages: {count_of_pages}, potential count of add to scrape: {__MAX_COUNT_OF_PROPERTIES}"
+#     def get_info_about_adds(self) -> None:
+#         """_summary_
+#         """
+#         count_of_pages = get_count_of_pages()
+#         ret = f"Count of pages: {count_of_pages}, potential count of add to scrape: {__MAX_COUNT_OF_PROPERTIES}"
     
-    def write_to_file(self, values: list) -> None:
-        file_name = os.path.join(self.keep_data_path, 'rooms1_' + datetime.datetime.now().strftime("%m_%d_%Y-%H:%M:%S") + '.txt')
-        with open(file_name, 'a+') as file:
-            [file.write(value + '\n') for value in values]
-        print(f"Urls file with name {file_name} saved into {self.keep_data_path}")
+#     def write_to_file(self, values: list) -> None:
+#         file_name = os.path.join(self.keep_data_path, 'rooms1_' + datetime.datetime.now().strftime("%m_%d_%Y-%H:%M:%S") + '.txt')
+#         with open(file_name, 'a+') as file:
+#             [file.write(value + '\n') for value in values]
+#         print(f"Urls file with name {file_name} saved into {self.keep_data_path}")
         
-    def scrape(self) -> None:
-        """_summary_
-        """
-        count_of_pages = self.get_count_of_pages()
-        if count_of_pages > self.__MAX_COUNT_OF_PAGE:
-            print(f"Count of pages is more then {__MAX_COUNT_OF_PAGE}. Scrapping only {__MAX_COUNT_OF_PAGE} pages")
-            count_of_pages = self.__MAX_COUNT_OF_PAGE
+#     def scrape(self) -> None:
+#         """_summary_
+#         """
+#         count_of_pages = self.get_count_of_pages()
+#         if count_of_pages > self.__MAX_COUNT_OF_PAGE:
+#             print(f"Count of pages is more then {__MAX_COUNT_OF_PAGE}. Scrapping only {__MAX_COUNT_OF_PAGE} pages")
+#             count_of_pages = self.__MAX_COUNT_OF_PAGE
             
-        print(f"Started scrapping. Count of pages: {count_of_pages}\n")
-        all_hrefs = []
-        for page in tqdm(range(1, count_of_pages + 1)):
-            url = self.start_url + f'&p={page}'
-            soup = BeautifulSoup(self.scrapper.get(url = url).text, 'html.parser')
-            objects = soup.findAll('a', {'class': '_93444fe79c--media--9P6wN'})
-            hrefs = [i.get('href') for i in objects]
-            all_hrefs.append(hrefs)
-            time.sleep(2)
-        all_hrefs = list(itertools.chain(*all_hrefs))
-        self.write_to_file(all_hrefs)
+#         print(f"Started scrapping. Count of pages: {count_of_pages}\n")
+#         all_hrefs = []
+#         for page in tqdm(range(1, count_of_pages + 1)):
+#             url = self.start_url + f'&p={page}'
+#             soup = BeautifulSoup(self.scrapper.get(url = url).text, 'html.parser')
+#             objects = soup.findAll('a', {'class': '_93444fe79c--media--9P6wN'})
+#             hrefs = [i.get('href') for i in objects]
+#             all_hrefs.append(hrefs)
+#             time.sleep(2)
+#         all_hrefs = list(itertools.chain(*all_hrefs))
+#         self.write_to_file(all_hrefs)
--- cian_parser/config.py
+++ cian_parser/config.py
@@ -1,7 +1,9 @@
 import os
-#requests params
+
+# ---------------------- requests params
 MAX_COUNT_OF_PAGE = 54
-MAX_COUNT_OF_ADDS = 28
+# MAX_COUNT_OF_ADDS = 28
+COUNT_PAGE_CLASS = '_93444fe79c--color_black_100--kPHhJ _93444fe79c--lineHeight_20px--tUURJ _93444fe79c--fontWeight_bold--ePDnv _93444fe79c--fontSize_14px--TCfeJ _93444fe79c--display_block--pDAEx _93444fe79c--text--g9xAG _93444fe79c--text_letterSpacing__normal--xbqP6'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML,[] like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 START_URL = 'https://kemerovo.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&region=4795&'
 API_JSON = {
@@ -31,16 +33,16 @@ API_JSON = {
 }
 API_ADDRESS = 'https://api.cian.ru/search-offers/v2/search-offers-desktop/'
 
-#mathcing
+# ---------------------- mathcing
 ROOMS_MATCHING_DICT = {'1': 'room1=1',
 '2': 'room2=1',
 '3': 'room3=1',
 '4': 'room4=1',
 '5': 'room5=1',
 '6': 'room6=1',
-'studio': 'room9=1'}
+'0': 'room9=1'}
 
-#paths
+# ---------------------- paths
 LOGS_PATH = 'logs'
-SCRAPE_LOG_PATH = os.path.join(LOGS_PATH, 'cian_scrapper.txt') 
+SCRAPE_LOG_PATH = os.path.join(LOGS_PATH, 'cian_scrapper.log')
 DATA_PATH = 'data'
